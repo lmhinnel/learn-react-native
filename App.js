402c6870031c91ffa9403bef0351a15811768214
lmhuong711 <lmhuong711@gmail.com>
2022-03-26 02:59:31 +0700

    list item with section and refreshable

--- App.js
+++ App.js
@@ -3,43 +3,63 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView,
   RefreshControl,
+  FlatList,
+  SectionList,
 } from 'react-native';
 
 const App = () => {
   const [items, setItems] = useState([
-    { key: 0, name: 'Item 0' },
-    { key: 1, name: 'Item 1' },
-    { key: 2, name: 'Item 2' },
+    {
+      title: 'Item 0',
+      data: ['Item 0.0'],
+    },
+    {
+      title: 'Item 1',
+      data: ['Item 1.0', 'Item 1.1'],
+    },
+    {
+      title: 'Item 2',
+      data: ['Item 2.0', 'Item 2.1', 'Item 2.2'],
+    },
   ]);
 
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = () => {
     setRefreshing(true);
-    setItems([...items, {key: items.length, name: 'Item ' + items.length}]);
+    var data = [];
+    const length = items.length;
+    for (var i = 0; i <= length; i++) {
+      data.push('Item ' + length + '.' + i);
+    }
+    setItems([...items, { title: 'Item ' + items.length, data: data }]);
     setRefreshing(false);
   }
 
   return (
     <View style={styles.body}>
-      <ScrollView
+      <SectionList
+        keyExtractor={(item, index) => index.toString()}
+        sections={items}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section }) => (
+          <View style={styles.title}>
+            <Text style={styles.text_title}>{section.title}</Text>
+          </View>
+        )}
         refreshControl={
           <RefreshControl
             refreshing={refreshing}
             onRefresh={onRefresh}
-            colors={['#d463AF']}
+            colors={['#6463AF']}
           />
-        }>
-        {items.map((item) => {
-          return (
-            <View style={styles.item} key={item.key}>
-              <Text style={styles.text}>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+        }
+      />
     </View>
   );
 };
@@ -48,20 +68,34 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 
   item: {
-    margin: 10,
     backgroundColor: '#6463AF',
     justifyContent: 'center',
     alignItems: 'center',
+    borderWidth: 0.5,
+  },
+
+  title: {
+    backgroundColor: '#1463AF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 3,
   },
 
   text: {
     color: '#fff',
-    fontSize: 36,
+    fontSize: 24,
     fontStyle: 'italic',
+    margin: 10,
+  },
+
+  text_title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
     margin: 10,
   }
 });
